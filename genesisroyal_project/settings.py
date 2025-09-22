d0b97fa395315d156a0960e49baddf329256e2ed
violetinfotech25 <violetinfotech25@gmail.com>
2025-09-22 17:54:13 +0530

    4th commit

--- genesisroyal_project/settings.py
+++ genesisroyal_project/settings.py
@@ -125,7 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Directory where collectstatic will copy all static files
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
