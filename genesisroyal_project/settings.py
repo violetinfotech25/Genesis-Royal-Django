3103094548e2b8622ecee553ae53244c8059bff0
violetinfotech25 <violetinfotech25@gmail.com>
2025-09-22 17:20:12 +0530

    3rd commit

--- genesisroyal_project/settings.py
+++ genesisroyal_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,13 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# These are optional but good to have
-STATICFILES_DIRS = [
-    BASE_DIR / "static",  # if you keep static files in /static
-]
-
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
